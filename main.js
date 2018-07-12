a13b4108cd5b7a238263b12218f5516595cf2c75
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-07-12 20:33:18 +1200

    Added comments to explain my code
    Added comments to explain my code

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 
 
 function outputFunction() {
+    //setting initial variables so i can change the html later on, plus i grab the value
+    //that a user has entered in the input field.
     var input = document.querySelector('input').value;
     outputText = document.getElementById("outputInt");
     outputTextFloat = document.getElementById("outputFloat");
     outputTextString = document.getElementById("outputString");
+    // below are some console.log statements to display in chrome. The connected one tells me that 
+    //my javascript is linked properly. the input returns what the user enters so i know it's being
+    //captured.
     console.log("connected");
     console.log(input)
     //this is the integer output!!
     var nonDecimal = parseInt(input);
+    //parse int, this transforms the text input (remember input type is set to text), to an integer. 
+    //as you can see, decimals are rounded. Text will return NaN because it's not a number.
     outputText.innerHTML = ("I'm the integer output!" + nonDecimal);
 
-    //this is the float output!!
+    //this is the float output!! floats use decimals e.g. 2.22 shows as 2.22
     var decimal = parseFloat(input);
     outputTextFloat.innerHTML = ("I'm the float output!" + decimal);
 
